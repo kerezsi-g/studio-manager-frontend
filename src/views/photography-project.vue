dac6bfe85e172bdc81ca247534249929b7b1b00d
kg <kerezsigergely@infobex.hu>
2025-05-09 16:05:13 +0200

    placeholder photography-project.vue

---
+++ src/views/photography-project.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { ProjectAsset, ProjectDetails } from '@/api-client'
+import { useModal } from '@/components/modal'
+import { ref } from 'vue'
+
+const props = defineProps<ProjectDetails>()
+
+const selectedAsset = ref<ProjectAsset | null>(null)
+
+const primaryAssets = computed(() => {
+  return props.assets.filter((asset) => asset.tag === 'pending-review')
+})
+
+const emit = defineEmits<{
+  (c: 'changed'): void
+}>()
+function handleSelectAsset(asset: ProjectAsset) {
+  selectedAsset.value = asset
+}
+</script>
+<template></template>
+<style lang="scss"></style>
